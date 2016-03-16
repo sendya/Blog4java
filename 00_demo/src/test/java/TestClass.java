d45a213e79941296535a37212c0dbe1bfe20d7a8
Sendya <18x@loacg.com>
2016-03-16 14:56:10 +0800

    Add marodkwn(pegdown)

--- 00_demo/src/test/java/TestClass.java
+++ 00_demo/src/test/java/TestClass.java
@@ -15,8 +15,15 @@
  * limitations under the License.
  */
 
+import org.pegdown.PegDownProcessor;
+import org.pegdown.ast.RootNode;
+import org.pegdown.plugins.PegDownPlugins;
+import org.pegdown.plugins.ToHtmlSerializerPlugin;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.jdbc.core.JdbcTemplate;
+
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * Project: blog4java
@@ -26,13 +33,27 @@ import org.springframework.jdbc.core.JdbcTemplate;
 public class TestClass {
 
     public static void main(String[] args)  {
-
+        /*
         ClassPathXmlApplicationContext
                 context = new ClassPathXmlApplicationContext("classpath:spring-db.xml");
         JdbcTemplate jdbcTemplate = context.getBean(JdbcTemplate.class);
-
+        */
 
         // jdbcTemplate.query("SELECT * FROM table WHERE id=?", new Object[] {1 });
+
+        // markdown test
+        String markdown = "## 我是H2  " +
+                "**加粗** ~~删除~~  " +
+                "hahha哈哈哈  " +
+                "-----------" +
+                "测试" +
+                "===========" +
+                "##### wtf?" +
+                "`function(){}`";
+        System.out.println(markdown);
+        PegDownProcessor pegDownProcessor = new PegDownProcessor(0);
+        String html = pegDownProcessor.markdownToHtml(markdown.toCharArray());
+        System.out.println(html);
 
     }
 }
